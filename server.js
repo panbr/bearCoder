4029e04f1922e877f82612372f0c36978a2c7d2e
panbaorui <115159694@qq.com>
2018-02-22 16:51:52 +0800

    add log

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
 
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
-var logger = require('morgan');
 var path = require('path');
+var fs = require('fs');
 var bodyParser = require('body-parser');
 var router = require('./routes/');
 var config = require('./config');
@@ -16,14 +16,23 @@ var config = require('./config');
 var app = module.exports = express();
 var db = new sqlite3.Database(__dirname+'/db/school.db');
 
+// log
+var log4js = require('./logger');
+var logger = log4js.getLogger();
+log4js.useLogger(app, logger);
 
 // Configuration
-app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'app')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function(req, res, next) {
+    if(JSON.stringify(req.body) !== '{}') {
+        logger.info({
+            time: new Date(),
+            req_params: req.body
+        });
+    } 
     req.db = db;
     next();
 });
