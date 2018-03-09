b13bb8b604bd5c8e5bcdd701ea8e5df0ca0d8189
panbr <115159694@qq.com>
2018-03-10 02:42:10 +0800

    add editor

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -21,6 +21,18 @@ class Index {
                 console.log('res: ', data)
             })
         })
+
+        // 运行代码编译器
+        $('#run').click(() => {
+            this._run();
+        });
+
+        // ctrl + Enter
+        $(document).keyup(event => { 
+            if(event.ctrlKey && event.keyCode === 13) { 
+                this._run();
+            } 
+        });
     }
 
     // 表单验证
@@ -43,6 +55,38 @@ class Index {
             }
         }
         return ret;
+    }
+
+    _run() {
+        let prog = document.getElementById("code").value;
+        console.log("...", prog);
+        let mypre = document.getElementById("output");
+        mypre.innerHTML = '';
+        Sk.pre = "output";
+        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+        Sk.configure({ output: this._outf, read: this._builtinRead });
+
+        let mp = Sk.misceval.asyncToPromise(function() {
+            return Sk.importMainWithBody("<stdin>", false, prog, true);
+        });
+        mp.then((mod) => {
+            console.log('success');
+        }, (err) => {
+            mypre.innerHTML = err.toString();
+            console.error(err.toString());
+        });
+    }
+
+    _builtinRead(x) {
+        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) {
+            throw "File not found: '" + x + "'";
+        }
+        return Sk.builtinFiles["files"][x];
+    }
+
+    _outf(text) { 
+        let mypre = document.getElementById("output"); 
+        mypre.innerHTML = mypre.innerHTML + text; 
     }
 
 }
